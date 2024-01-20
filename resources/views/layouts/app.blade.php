f3c3f5084f8501a6978f4fc5d6f171a963fb268e
dejan-prole <shimids89@gmail.com>
2024-01-20 04:54:45 +0100

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -199,7 +199,7 @@
                                             <!--begin::Col-->
                                             <div class="col-6">
                                                 <!--begin::Link-->
-                                                <a href="{{ route('licenses.index') }}" class="@if (Route::currentRouteName() === 'licenses.index') btn-light-primary @endif btn btn-icon btn-outline btn-bg-light btn-active-light-primary btn-flex flex-column flex-center w-100px h-100px border-gray-200" data-kt-button="true">
+                                                <a href="javascript:void()" class="@if (Route::currentRouteName() === 'orders.index') btn-light-primary @endif btn btn-icon btn-outline btn-bg-light btn-active-light-primary btn-flex flex-column flex-center w-100px h-100px border-gray-200" data-kt-button="true">
                                                     <!--begin::Icon-->
                                                     <span class="mb-2">
                                                         <i class="ki-outline ki-purchase fs-1"></i>
