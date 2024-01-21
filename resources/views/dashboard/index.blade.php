a5e95aa0063eabff5ef3fe9df57ebc1bc19a99b6
dejan-prole <shimids89@gmail.com>
2024-01-21 21:57:25 +0100

    Update index.blade.php

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -124,7 +124,7 @@
                         <!--begin::Symbol-->
                         <div class="symbol symbol-45px">
                             <span class="symbol-label bg-light-info">
-                                <i class="ki-outline ki-handcart fs-2x text-gray-800"></i>
+                                <i class="ki-outline ki-purchase fs-2x text-gray-800"></i>
                             </span>
                         </div>
                         <!--end::Symbol-->
