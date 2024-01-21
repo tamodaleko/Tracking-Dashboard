36bb0e4cc1218877cd6d99e73928506175db1910
dejan-prole <shimids89@gmail.com>
2024-01-21 08:18:52 +0100

    Delete test order

--- app/Http/Controllers/WebhooksController.php
+++ app/Http/Controllers/WebhooksController.php
@@ -21,8 +21,6 @@ class WebhooksController extends Controller
 
         $data = json_decode($request->getContent(), true);
 
-        \Illuminate\Support\Facades\Log::emergency('Order ID: ' . $data['id']);
-
         if ($topic === 'orders/create') {
             $this->createShopifyOrder($company, $data);
         }
@@ -38,15 +36,9 @@ class WebhooksController extends Controller
             return false;
         }
 
-        \Illuminate\Support\Facades\Log::emergency($secret);
-
         $signature = $request->header('X-Shopify-Hmac-Sha256');
 
-        \Illuminate\Support\Facades\Log::emergency($signature);
-
         $calculateSignature = base64_encode(hash_hmac('sha256', $request->getContent(), $secret, true));
-
-        \Illuminate\Support\Facades\Log::emergency($calculateSignature);
 
         return hash_equals($signature, $calculateSignature);
     }
