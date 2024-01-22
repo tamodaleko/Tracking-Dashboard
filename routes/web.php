ed2cc695def532bf09b48987ce5bfb0f3ca68e7c
dejan-prole <shimids89@gmail.com>
2024-01-22 03:38:24 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -77,7 +77,7 @@ Route::get('/test', function () {
     //   dd($campaigns);
     // }
     
-    dd(\App\Models\Campaign\CampaignStat::all());
+    dd(\App\Models\Company::all());
     
     $api = \FacebookAds\Api::init('1409959359610602', '7709b913e276c61a737f7fe081890061', 'EAAUCWb7GZCuoBOZC5otBpMce7zvE5TUKFjHy7dKQn3D8xzpCjuSTa2CXMaLDvFyFlODV5SaZBKTE4di0duXLFdPgZAFQX05Q9x6EGzE698pWICFrZA4k9OtxNrGQ7CGO1KzwK6fanUoHm1QlBlcZAViWduTXWOQhhpri2ZBwwbopWUZBnoqpuNSSIOuCaGPe4PGP');
 });
