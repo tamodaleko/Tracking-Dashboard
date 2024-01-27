3175e3f8a52c469374e0486a2c1135772824c7f8
dejan-prole <shimids89@gmail.com>
2024-01-27 23:49:49 +0100

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         $schedule->command('app:update-campaign-stats')->hourlyAt(58)->withoutOverlapping();
-        $schedule->command('app:update-products')->everyFiveMinutes()->withoutOverlapping();
+        $schedule->command('app:update-products')->everySixHours()->withoutOverlapping();
     }
 
     /**
